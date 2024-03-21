7281455eda9621cd063042fbf99bc223d73d26a0
quangbaort <quangbaort@gmail.com>
2024-03-21 14:05:37 +0700

    develop

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->string('user_name')->unique();
             $table->string('image')->nullable();
             $table->boolean('is_admin')->default(false);
